40b6b6081eb5af726a7317a6839a8c32735e6231
arsmoriendy <nugrahasteven@gmail.com>
2026-06-24 20:14:38 +0700

    feat: test missing, invalid and extra entries

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,29 +1,30 @@
 use super::*;
 
-#[test]
-fn valid() {
-    with_runtime(async {
-        let project_dir = env!("CARGO_MANIFEST_DIR");
+macro_rules! state {
+    (csv = $csv:expr, dir = $dir:expr) => {{
+        let project_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/tests");
         let (csv, dir) = (
-            Path::new(project_dir).join("src/tests/assets/dir1_sum.csv"),
-            Path::new(project_dir).join("src/tests/assets/dir1"),
+            project_dir.clone().join($csv),
+            project_dir.clone().join($dir),
         );
 
         let entries = Entry::list(&csv).await.unwrap();
 
-        let state = State {
+        Arc::new(Mutex::new(State {
             csv_path: csv.to_owned().into_os_string(),
             dir_path: dir.to_owned().into_os_string(),
             entries,
             ..Default::default()
-        };
+        }))
+    }};
+}
 
-        let state = Arc::new(Mutex::new(state));
-
-        check(state.clone()).await.unwrap();
+macro_rules! check {
+    ($state:expr) => {
+        check($state.clone()).await.unwrap();
 
         loop {
-            let state = state.lock().await;
+            let state = $state.lock().await;
             let validation_progress = state.validation_progress;
             let validation_length = state.entries.len();
             if validation_progress == validation_length {
@@ -32,12 +33,69 @@ fn valid() {
             drop(state);
             sleep(Duration::from_millis(100)).await;
         }
+    };
+}
 
-        let state = Arc::try_unwrap(state).unwrap().into_inner();
+macro_rules! checked_state {
+    (csv = $csv:expr, dir = $dir:expr) => {{
+        let state = state!(csv = $csv, dir = $dir);
+
+        check!(state);
+
+        Arc::try_unwrap(state).unwrap().into_inner()
+    }};
+}
+
+#[test]
+fn valid() {
+    with_runtime(async {
+        let state = checked_state!(csv = "./assets/dir1_sum.csv", dir = "./assets/dir1/");
 
         assert_eq!(state.extra.len(), 0);
         assert_eq!(state.missing.len(), 0);
         assert_eq!(state.invalid.len(), 0);
         assert_eq!(state.valid.len(), 17);
+    });
+}
+
+#[test]
+fn extra() {
+    with_runtime(async {
+        let state = checked_state!(csv = "./assets/dir1_extra_sum.csv", dir = "./assets/dir1/");
+
+        assert_eq!(state.extra.len(), 3);
+        assert_eq!(state.missing.len(), 0);
+        assert_eq!(state.invalid.len(), 0);
+        assert_eq!(state.valid.len(), 17 - 3);
+    });
+}
+
+#[test]
+fn invalid() {
+    with_runtime(async {
+        let state = checked_state!(
+            csv = "./assets/dir1_invalid_sum.csv",
+            dir = "./assets/dir1/"
+        );
+
+        assert_eq!(state.extra.len(), 0);
+        assert_eq!(state.missing.len(), 0);
+        assert_eq!(state.invalid.len(), 3);
+        assert_eq!(state.valid.len(), 17 - 3);
+    });
+}
+
+#[test]
+fn missing() {
+    with_runtime(async {
+        let state = checked_state!(
+            csv = "./assets/dir1_missing_sum.csv",
+            dir = "./assets/dir1/"
+        );
+
+        assert_eq!(state.extra.len(), 0);
+        assert_eq!(state.missing.len(), 2);
+        assert_eq!(state.invalid.len(), 0);
+        assert_eq!(state.valid.len(), 19 - 2);
     });
 }
